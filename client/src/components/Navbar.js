54bb4b30a4707e4729173b29cd69d21e09cc6ee9
Austin J FitzHugh <austin.j.fitzhugh@gmail.com>
2022-02-06 16:47:42 -0500

    logo

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Navbar = () => {
+  return <div> Navigate Bar</div>;
+};
+
+export default Navbar;
---
+++ client/src/components/Values.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Values = () => {
+  return <div>Values go here</div>;
+};
+
+export default Values;
